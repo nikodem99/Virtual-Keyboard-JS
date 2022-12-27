aacdad032e91f4e5593125b1287b6acf44aab7f8
Nikodem <cwikla.nikodem@gmail.com>
2022-12-27 03:49:35 +0100

    commit
    one buttons working but rest don't work, you need fixing this. Keep working on this project it need fixing

---
+++ dist/index.7210d5f9.js
@@ -0,0 +1,32 @@
+const $key = (key)=>document.querySelector(`kbd[data-key='${key}'], kbd[data-alt='${key}']`);
+const codeToElement = {
+    "CapsLock": $key("caps"),
+    "Space": $key("space"),
+    "Backslash": document.getElementById("backslash"),
+    "Quote": document.getElementById("quote"),
+    "ShiftLeft": $key("lshift"),
+    "ShiftRight": $key("rshift"),
+    "ControlLeft": $key("lctrl"),
+    "ControlRight": $key("rctrl"),
+    "AltLeft": $key("lalt"),
+    "AltRight": $key("ralt"),
+    "MetaLeft": $key("lwin"),
+    "MetaRight": $key("rwin")
+};
+window.addEventListener("keydown", (e)=>{
+    console.log(e);
+    const el = codeToElement[e.code] || $key(e.key.toLowerCase());
+    if (el) {
+        el.classList.add("pressed");
+        e.preventDefault();
+    }
+});
+window.addEventListener("keyup", (e)=>{
+    const el = codeToElement[e.code] || $key(e.key.toLowerCase());
+    if (el) {
+        el.classList.remove("pressed");
+        e.preventDefault();
+    }
+});
+
+//# sourceMappingURL=index.7210d5f9.js.map
